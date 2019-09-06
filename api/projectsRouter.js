7ff89887c05bebc510e5dd1d3ae6806a5a032648
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-06 10:02:52 -0700

    Validate ID middleware added

--- api/projectsRouter.js
+++ api/projectsRouter.js
@@ -13,15 +13,38 @@ router.get("/", (req, res) => {
         })
 })
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateProjectId, (req, res) => {
     console.log("Get to projects with ID works")
     const id = req.params.id;
+    console.log("id in get: ", db.get(id))
     db.get(id)
         .then(projects => res.status(201).json(projects) )
         .catch( () => {
             res.status(500).json({ error: "The project information could not be retrieved." })
         })
 })
+
+// router.post("")
+
+
+
+//custom middleware
+function validateProjectId(req, res, next) {
+    let id = req.params.id;
+    db.getById(id)
+        .then(project => {
+            if(project) {
+                console.log("after then", project.id)
+                req.project = project
+                next()
+            } else {
+                res.status(404).json({error: "No project with that ID exists"})
+            }
+        })
+        .catch( () => {
+            res.status(500).json({error: "Server Error"})
+        })
+};
 
 
 module.exports = router;
